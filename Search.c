f1d495f4c7152e89b7ef8c53d17602dc14ff580c
jannatulferdowse07-dsa <jannatulferdowse07@gmail.com>
2026-07-30 21:03:18 +0600

    Linear_Search.c

---
+++ Search.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int linearSearch(int arr[], int size, int key) {
+    for (int i = 0; i < size; i++) {
+        if (arr[i] == key) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int main() {
+    int arr[] = {12, 45, 7, 23, 9, 56};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int key = 23;
+
+    int index = linearSearch(arr, size, key);
+
+    if (index != -1) {
+        printf("Element found at index: %d\n", index);
+    } else {
+        printf("Element not found!\n");
+    }
+
+    return 0;
+}
